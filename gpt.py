e6a327f061cf28a4d6293f475fa980d1ac4b71bd
Liam Gibbons <112109780+lgibbons12@users.noreply.github.com>
2023-01-30 22:05:00 -0500

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,15 +1,17 @@
+#imports
 import re
-
 import random
 import requests
 
-#you just have to shift ctrl v to enter it in
+#get ChatGPT input
 text =  input("Please Enter ChatGPT Text Here: ")
 
 num_of_inputs = input("How Many Words To Change? (Type All For All Words to Change) ")
 
-
+#splits the text into a array of words
 indices = re.split(" ", text)
+
+#logic to make sure num_of_inputs is an integer
 if num_of_inputs == "All":
     num_of_inputs = len(indices)
 else:
@@ -17,7 +19,9 @@ else:
 if num_of_inputs > len(indices):
     num_of_inputs = len(indices)
 
+#main loop to edit text
 for i in range(num_of_inputs):
+    #pick a random word from the array
     r = random.randint(0, len(indices))
     if 0 <= r < len(indices):
         pass
@@ -25,17 +29,20 @@ for i in range(num_of_inputs):
         r = len(indices) - 1
         print(indices[r])
     
+    #sends request to api to find a synonym for the selected word
     api_url = 'https://api.api-ninjas.com/v1/thesaurus?word={}'.format(indices[r])
     response = requests.get(api_url, headers={'X-Api-Key': 'Ra4kIEhA16TBeT1DhQz8pQ==kcNK1VKJk5y6QxPO'})
     if response.status_code == requests.codes.ok:
         
+        #use re to grab out the synonym
         search = re.findall('\["(.*?)"', response.text)
         
+        #passes if there were no synonyms for the word
         if len(search) > 0:
             indices[r] = search[0]
-            print("wiaufhwophfiupowehfoupwehfwuh")
         else:
             pass
+        #simple logic to give updates on percent completed
         percent = round(i/num_of_inputs, 2) * 100
         print(f"{percent}% Complete")
 
@@ -43,9 +50,11 @@ for i in range(num_of_inputs):
 
     else:
         print("Error:", response.status_code, response.text)
-   
+  
+#puts the array back into text
 indices = " ".join(indices)
 
+#shows newly formed text!
 print(indices)
 
 
